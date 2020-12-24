1b9a10c6aca0d848be7ed8a0f78bbf825dfb1aaa
Maurilio <4431@sky.com.br>
2020-12-24 10:06:23 -0300

    Otimizando o javascript

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+const $header = document.querySelector('header');
+
+window.addEventListener('scroll',toggleHeader,false);
+
+function toggleHeader() {
+    if (window.pageYOffset > 60 && $header.classList.contains('max-header')) {
+        $header.classList.remove('max-header');
+        $header.classList.add('min-header');
+    } else if (window.pageYOffset <= 60 && $header.classList.contains('min-header')) {
+        $header.classList.add('max-header');
+        $header.classList.remove('min-header');
+    }
+}
